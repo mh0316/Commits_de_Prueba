8e73d4afd39903820360c709269c92f330db67d6
Marcelo <m.henriquez11@ufromail.cl>
2022-06-28 00:03:06 -0400

    Tercer commit

--- src/main/java/Ejemplo.java
+++ src/main/java/Ejemplo.java
@@ -2,5 +2,7 @@ public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Actualización del programa");
+
+        System.out.println("Hasta luego");
     }
 }
